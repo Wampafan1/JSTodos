70be52bd1f85781e00f5cf87d1f754aa8ebb5fb3
root <delghing@gmail.com>
2023-10-05 17:04:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('#todos');
+const input = document.querySelector('#todo');
+const todoList = document.querySelector('#todo-list');
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+// Function to render todos to the DOM
+const renderTodos = () => {
+    todoList.innerHTML = '';
+    todos.forEach((todo, index) => {
+        const newTodo = document.createElement('li');
+        const removeBtn = document.createElement('button');
+        removeBtn.innerText = 'Remove';
+        newTodo.innerText = todo + '    ';
+        newTodo.setAttribute('data-index', index);
+        newTodo.appendChild(removeBtn)
+        todoList.appendChild(newTodo);
+    });
+};
+
+// Initial render
+renderTodos();
+
+todoList.addEventListener('click', function(e){
+    if (e.target.tagName === 'BUTTON'){
+        const todoText = e.target.parentElement.innerText.trim();
+        const index = e.target.parentElement.getAttribute('data-index');
+        
+        if (todoText) {
+            todos.splice(index, 1);
+            localStorage.setItem('todos', JSON.stringify(todos));
+            renderTodos();
+        }
+        e.target.parentElement.remove();
+    }
+    else if (e.target.tagName === 'LI'){
+        if (e.target.classList.contains('strike-through')) {
+            e.target.classList.remove('strike-through');
+        } else {
+            e.target.classList.add('strike-through');
+        }
+    }
+})
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const todoText = input.value.trim();
+    
+    if (todoText) {
+        todos.push(todoText);
+        localStorage.setItem('todos', JSON.stringify(todos));
+        renderTodos();
+    }
+    
+})
